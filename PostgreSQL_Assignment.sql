f9bc7a5b80f829a0448ad5a638ca20824a863c1c
Naeem Muntasir <naeemtasir03@gmail.com>
2025-05-31 00:27:00 +0600

    solved problem-1

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -44,3 +44,7 @@ INSERT INTO sightings(species_id,ranger_id,location,sighting_time,notes)VALUES
 (3,3,'Bamboo Grove East','2024-05-15 09:10:00','Feeding observed '),
 (1,2,'Snowfall Pass','2024-05-18 18:30:00',NULL);
 
+
+-- Problem: 1. Insert data
+INSERT INTO rangers(name, region) VALUES
+('Derek Fox', 'Coastal Plains');
